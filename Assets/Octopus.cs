845980378eb4096fcbdc5f43461c2f0927895f61
Ideae <zack.tibia@gmail.com>
2015-05-03 00:19:15 -0400

    :/

---
+++ Assets/Octopus.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Octopus : MonoBehaviour {
+    public Cell currentCell;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetCell(int x, int y)
+    {
+        Cell next = RoomManager.Get(x, y);
+        if (next != null)
+        {
+            currentCell = next;
+            transform.position = new Vector3(currentCell.x, currentCell.y);
+        }
+    }
+}
