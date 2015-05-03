b123de6adfc49c7c5d569035128c4dc637b0dd26
ColgateCavityPatrol <i.alexei@hotmail.com>
2015-05-03 06:46:41 -0400

    all the things

--- Assets/TitleScreen.cs
+++ Assets/TitleScreen.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class TitleScreen : MonoBehaviour
 {
     public string firstLevel;
+	public GameObject StartButton;
+	public GameObject WhatDoButton;
+	public GameObject Logo;
+	public GameObject Credits;
+	public GameObject WhatDoPanel;
+	public GameObject Xbutton;
+	public GameObject RulesText;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -18,4 +27,20 @@ public class TitleScreen : MonoBehaviour
         Debug.Log("Click!");
         FileWrite.InitDeserialization(firstLevel + ".xml");
     }
+	public void HowTo() {
+		StartButton.SetActive (false);
+		WhatDoButton.SetActive (false);
+		Logo.SetActive (false);
+		Credits.SetActive (false);
+		WhatDoPanel.SetActive (true);
+
+	}
+	public void ExitRules() {
+		WhatDoPanel.SetActive (false);
+		StartButton.SetActive (true);
+		WhatDoButton.SetActive (true);
+		Logo.SetActive (true);
+		Credits.SetActive (true);
+
+	}
 }
